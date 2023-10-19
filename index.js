a67b8a66b769f0aa4c8ad72854c0dbebb207e7c4
Abu Hosain <cryptichasan925@gmail.com>
2023-10-20 03:42:42 +0600

    server site completed

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const cors = require('cors');
+app.use(cors())
+const port = 3000;
+const chefs = require('./data/chefs.json');
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+});
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
 })
+
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const selectedChef = chefs.find(n => n._id == id);
+    res.send(selectedChef)
+}) 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
